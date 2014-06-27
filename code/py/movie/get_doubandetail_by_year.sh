6f1bace4bda7f24d8a628ecfa62083306aa30c3d
zeuswang <zeuswoody@qq.com>
2014-06-27 11:27:14 +0800

    by wangwei

---
+++ code/py/movie/get_doubandetail_by_year.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+year=$1
+url_dir= /data/wangwei/movie/spider/url/
+res_dir= /data/wangwei/movie/spider/result/douban/
+if [ ! -d ${res_dir}${year}/ ]
+then
+	mkdir ${res_dir}${year}
+fi
+nohup python detail.py ${url_dir}${year}/ ${res_dir}${year}/ > doubani_detial_${year}.log &
+
+
